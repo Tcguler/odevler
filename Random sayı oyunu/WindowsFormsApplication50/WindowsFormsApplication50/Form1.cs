fddc88792887ff994f973ae45e9a5a0bdac0b9d1
stuna31 <49326222+stuna31@users.noreply.github.com>
2019-04-06 02:04:45 +0300

    Add files via upload

---
+++ Random sayı oyunu/WindowsFormsApplication50/WindowsFormsApplication50/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication50
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayi;
+        Random rnd = new Random();
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sayi = rnd.Next(0,100);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int girilensayi = Convert.ToInt32(textBox1.Text);
+
+            if (sayi == girilensayi)
+            {
+                label2.Text = "Doğru Sayı!";
+            }
+            if(sayi<girilensayi)
+            {
+                label2.Text="Daha Küçük Sayı Tahmin Et!";
+            }
+            if (sayi > girilensayi)
+            {
+                    label2.Text="Daha Büyük Sayı Tahmin Et!";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ RandomTC/RandomTC/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RandomTC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            Random rnd = new Random();
+            int b = Convert.ToInt32(textBox1.Text);
+
+            
+            for (int a = 0; a < b; a++)
+            {
+                sayac += 1;
+                int[] dizi = new int[9];
+                for (int j = 0; j < 11; j++)
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        dizi[i] = rnd.Next(0, 10);
+                        if (dizi[0] == 0)
+                            i--;
+                    }
+                }
+
+                string tcKimlik = dizi.Aggregate("", (current, t) => current + t);
+                int tekBasamakToplam = 0;
+                int ciftBasamakToplam = 0;
+                int tekilHesap;
+                for (int i = 0; i < 9; i += 2)
+                    tekBasamakToplam += int.Parse(tcKimlik.Substring(i, 1));
+                for (int i = 1; i < 9; i += 2)
+                    ciftBasamakToplam += int.Parse(tcKimlik.Substring(i, 1));
+                tekilHesap = ((tekBasamakToplam * 7) - ciftBasamakToplam) % 10;
+                tcKimlik += Convert.ToInt32(tekilHesap);
+                int toplam = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    byte toplanacakSayi = byte.Parse(tcKimlik.Substring(i, 1));
+                    toplam += toplanacakSayi;
+                    toplam %= 10;
+                }
+                tcKimlik += Convert.ToInt32(toplam);
+
+                listBox1.Items.Add(tcKimlik);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
