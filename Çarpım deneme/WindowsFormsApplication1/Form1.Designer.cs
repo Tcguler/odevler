7e72591c152c6eeb73f92edfa6494cd500f11f61
stuna31 <49326222+stuna31@users.noreply.github.com>
2019-05-16 22:02:00 +0300

    Add files via upload

---
+++ Çarpım deneme/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSatir = new System.Windows.Forms.TextBox();
+            this.txtSutun = new System.Windows.Forms.TextBox();
+            this.MatrisA = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MatrisB = new System.Windows.Forms.DataGridView();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtSonuc = new System.Windows.Forms.DataGridView();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.MatrisA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MatrisB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSonuc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(438, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Çarp";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSatir
+            // 
+            this.txtSatir.Location = new System.Drawing.Point(100, 76);
+            this.txtSatir.Name = "txtSatir";
+            this.txtSatir.Size = new System.Drawing.Size(100, 20);
+            this.txtSatir.TabIndex = 4;
+            // 
+            // txtSutun
+            // 
+            this.txtSutun.Location = new System.Drawing.Point(677, 76);
+            this.txtSutun.Name = "txtSutun";
+            this.txtSutun.Size = new System.Drawing.Size(100, 20);
+            this.txtSutun.TabIndex = 5;
+            // 
+            // MatrisA
+            // 
+            this.MatrisA.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MatrisA.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.MatrisA.Location = new System.Drawing.Point(32, 181);
+            this.MatrisA.Name = "MatrisA";
+            this.MatrisA.Size = new System.Drawing.Size(287, 150);
+            this.MatrisA.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "3";
+            this.Column3.Name = "Column3";
+            // 
+            // MatrisB
+            // 
+            this.MatrisB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MatrisB.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.MatrisB.Location = new System.Drawing.Point(363, 181);
+            this.MatrisB.Name = "MatrisB";
+            this.MatrisB.Size = new System.Drawing.Size(277, 150);
+            this.MatrisB.TabIndex = 7;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "1";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "2";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "3";
+            this.Column6.Name = "Column6";
+            // 
+            // txtSonuc
+            // 
+            this.txtSonuc.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.txtSonuc.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column7,
+            this.Column8,
+            this.Column9});
+            this.txtSonuc.Location = new System.Drawing.Point(677, 181);
+            this.txtSonuc.Name = "txtSonuc";
+            this.txtSonuc.Size = new System.Drawing.Size(297, 150);
+            this.txtSonuc.TabIndex = 8;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "1";
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "2";
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "3";
+            this.Column9.Name = "Column9";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 415);
+            this.Controls.Add(this.txtSonuc);
+            this.Controls.Add(this.MatrisB);
+            this.Controls.Add(this.MatrisA);
+            this.Controls.Add(this.txtSutun);
+            this.Controls.Add(this.txtSatir);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.MatrisA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MatrisB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSonuc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSatir;
+        private System.Windows.Forms.TextBox txtSutun;
+        private System.Windows.Forms.DataGridView MatrisA;
+        private System.Windows.Forms.DataGridView MatrisB;
+        private System.Windows.Forms.DataGridView txtSonuc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+    }
+}
+
---
+++ Çarpım deneme/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+       
+        Random salla = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool Matris_Al(string str, int[,] dizi)
+        {
+            string[] s;
+            char[] bölücüler = { ' ', '\r' };
+            s = str.Split(bölücüler);   
+            if (s.GetUpperBound(0) < (Convert.ToInt32(txtSatir.Text) * Convert.ToInt32(txtSutun.Text) ) );
+{
+    MessageBox.Show("Matris yanlış girilmiştir");
+return false;
+}
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(txtSatir.Text);
+            int y = Convert.ToInt32(txtSutun.Text);
+            int[,] a = new int[x, y];
+            int[,] b = new int[x, y];
+            int[,] c = new int[x, y];
+            int s = 0; 
+
+            Matris_Al(MatrisA.Text, a);
+            Matris_Al(MatrisB.Text, b);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    s = 0;
+                    for (int k = 0; k < x; k++)
+                    {
+                        s = s + a[i, k] * b[k, i];
+                    }
+                    c[i, j] = s;
+                }
+            }
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    txtSonuc.Text = txtSonuc.Text + " " + c[i, j].ToString();
+                }
+                txtSonuc.Text += "\r\n";
+            }
+        }
+    }
+}
